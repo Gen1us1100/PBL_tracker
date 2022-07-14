0dfad2ee0f095799fb43aec6ec8cb1066d2d5b82
Sam <coder.sam.1100@gmail.com>
2022-07-14 13:56:52 +0530

    Added Registration form ( CSS needs to be fixed)

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
      <form action="login.php" method="post">
-	 	<div class="money"><img src ="money.jpg" class="img"></img></div>
+	 	<div class="money"><img src ="money.jpg" class="money"></img></div>
      	<h2>LOGIN</h2>
      	<?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
@@ -18,6 +18,33 @@
      	<input type="password" name="password" placeholder="Password"><br>
 
      	<button type="submit">Login</button>
-     </form>
+	 </form>
+	 <form action="action_page.php" style="border:1px solid #ccc">
+  <div class="container">
+    <h1>Sign Up</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+    <label>
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label>
+
+    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
 </body>
 </html>
